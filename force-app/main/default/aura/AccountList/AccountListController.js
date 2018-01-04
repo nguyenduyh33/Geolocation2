9369b10c9678468743c51652920a7b8fef57dd08
Duy Nguyen <duy.nguyen@prolifiq.com>
2018-01-03 16:20:09 -0800

    Added pins to map

--- force-app/main/default/aura/AccountList/AccountListController.js
+++ force-app/main/default/aura/AccountList/AccountListController.js
@@ -3,6 +3,9 @@
         var action = component.get("c.findAll");
         action.setCallback(this, function (a) {
             component.set("v.accounts", a.getReturnValue());
+            var event = $A.get("e.c:AccountsLoaded");
+            event.setParams({ "accounts": a.getReturnValue() });
+            event.fire();
         });
         $A.enqueueAction(action);
     }
--- force-app/main/default/aura/AccountMap/AccountMapController.js
+++ force-app/main/default/aura/AccountMap/AccountMapController.js
@@ -7,5 +7,17 @@
                 attribution: 'Tiles © Esri'
             }).addTo(map);
         component.set("v.map", map);
+    },
+
+    accountsLoaded: function (component, event, helper) {
+
+        // Add markers
+        var map = component.get('v.map');
+        var accounts = event.getParam('accounts');
+        for (var i = 0; i < accounts.length; i++) {
+            var account = accounts[i];
+            var latLng = [account.Location__Latitude__s, account.Location__Longitude__s];
+            L.marker(latLng, { account: account }).addTo(map);
+        }
     }
 })
